75f2e4bdc6eaac39c77d04b528b589195302485c
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-07-05 12:45:22 +0530

    Added global styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,11 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Header */}
-        <Navbar />
-        {children}
-        {/* Footer */}
-        <Footer />
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
