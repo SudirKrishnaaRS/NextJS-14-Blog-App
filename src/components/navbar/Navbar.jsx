5731456233c65cc76feefed494169c25bb7e264c
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-07-05 11:20:08 +0530

    Added Navbar Links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Links from "./links/Links";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
