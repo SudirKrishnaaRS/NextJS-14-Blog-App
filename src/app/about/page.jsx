eb7c5284ba06dabec071c752be1cd36ad42f44b8
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-07-08 13:39:00 +0530

    NextJS Image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        {/* Traditional */}
+        {/* <img src="./about.png" /> */}
+
+        {/* NextJS Image tag */}
+        {/* <Image src="/about.png" width={500} height={500} /> */}
+
+        {/* NextJS Image tag : When used inside a container */}
+        {/* <Image src="/about.png" fill /> */}
+
+        {/* NextJS Image tag : Handling External Images */}
+        <Image
+          src="https://images.pexels.com/photos/15804651/pexels-photo-15804651/free-photo-of-people-together-on-motorcycle-on-road-in-mountains.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
