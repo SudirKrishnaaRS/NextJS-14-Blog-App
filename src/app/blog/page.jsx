6a82bc7773a7bde717d252ca7159b829c1b1de46
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-07-16 12:42:56 +0530

    Data fecthing in Blog post page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,21 +1,34 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
 
-const BlogPage = () => {
+const getData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    // cache: "no-store",
+    next: { revalidate: 3600 },
+  });
+  // NOTE:
+  // - cache: NextJS by default caches the API responses, if don't want then explicitly mention
+  // - next: It helps in caching and the you can revalidate after a particular duration like eg) each hour - 3600 sec
+  if (!res.ok) {
+    throw new Error("Something went wrong, Please try again");
+  }
+
+  return res.json();
+};
+
+const BlogPage = async () => {
+  const posts = await getData();
+
+  // console.log("first0", posts);
   return (
     <div className={styles.container}>
-      <div className={styles.post}>
-        <PostCard />
-      </div>
-      <div className={styles.post}>
-        <PostCard />
-      </div>{" "}
-      <div className={styles.post}>
-        <PostCard />
-      </div>{" "}
-      <div className={styles.post}>
-        <PostCard />
-      </div>
+      {posts.map((post) => {
+        return (
+          <div className={styles.post} key={post.id}>
+            <PostCard post={post} />
+          </div>
+        );
+      })}
     </div>
   );
 };
