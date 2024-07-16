cba61790d4d1bc8b1dfc088ad05de094690fc444
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-07-16 12:43:23 +0530

    Dynamic data based on API response in PostCard Component

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./postCard.module.css";
 import Link from "next/link";
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -16,12 +16,9 @@ const PostCard = () => {
         <span className={styles.date}>01.01.2024</span>
       </div>
       <div className={styles.bottom}>
-        <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum illo
-          quo odio alias omnis sit minima eos molestiae veritatis perferendis?
-        </p>
-        <Link href="/blog/post" className={styles.link}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>{post.body}</p>
+        <Link href={`/blog/${post.id}`} className={styles.link}>
           Read More
         </Link>
       </div>
