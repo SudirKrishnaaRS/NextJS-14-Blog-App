a9c2fd7d8091cda55d8d4772dcdb426d85ee9c42
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-08-07 13:01:44 +0530

    Next.js 14 How to Fetch Data without Using an API

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -2,23 +2,29 @@ import Image from "next/image";
 import styles from "./singlePost.module.css";
 import PostUser from "@/components/postUser/postUser";
 import { Suspense } from "react";
+import { getPost } from "@/lib/data";
 
-const getData = async (slug) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+// FETCH DATA WITH AN API
+// const getData = async (slug) => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
 
-  if (!res.ok) {
-    throw new Error("Something went wrong, Please try again");
-  }
+//   if (!res.ok) {
+//     throw new Error("Something went wrong, Please try again");
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 
 const SinglePostPage = async ({ params }) => {
   console.log("Params", params);
 
   const { slug } = params;
 
-  const post = await getData(slug);
+  // FETCH DATA WITH AN API
+  // const post = await getData(slug);
+
+  // FETCH DATA WITHOUT AN API
+  const post = await getPost(slug);
 
   return (
     <div className={styles.container}>
@@ -32,7 +38,7 @@ const SinglePostPage = async ({ params }) => {
       </div>
 
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post.title}</h1>
+        <h1 className={styles.title}>{post?.title}</h1>
         <div className={styles.detail}>
           <Image
             src="https://images.pexels.com/photos/23383398/pexels-photo-23383398/free-photo-of-tuscany-in-spring.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
@@ -41,16 +47,18 @@ const SinglePostPage = async ({ params }) => {
             height={50}
             className={styles.avatar}
           />
-          <Suspense fallback={<div>Loading...</div>}>
-            <PostUser userId={post.userId} />
-          </Suspense>
+          {post && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>01.01.2024</span>
           </div>
         </div>
 
-        <div className={styles.content}>{post.body}</div>
+        <div className={styles.content}>{post?.body}</div>
       </div>
     </div>
   );
