e6b4d98032c77986de17b26181d46590326ba42e
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-08-07 13:01:37 +0530

    Next.js 14 How to Fetch Data without Using an API

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,25 +1,30 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
+import { getPosts } from "@/lib/data";
 
-const getData = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    // cache: "no-store",
-    next: { revalidate: 3600 },
-  });
-  // NOTE:
-  // - cache: NextJS by default caches the API responses, if don't want then explicitly mention
-  // - next: It helps in caching and the you can revalidate after a particular duration like eg) each hour - 3600 sec
-  if (!res.ok) {
-    throw new Error("Something went wrong, Please try again");
-  }
+// FETCH DATA WITH AN API
+// const getData = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//     // cache: "no-store",
+//     next: { revalidate: 3600 },
+//   });
+//   // NOTE:
+//   // - cache: NextJS by default caches the API responses, if don't want then explicitly mention
+//   // - next: It helps in caching and the you can revalidate after a particular duration like eg) each hour - 3600 sec
+//   if (!res.ok) {
+//     throw new Error("Something went wrong, Please try again");
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 
 const BlogPage = async () => {
-  const posts = await getData();
+  // FETCH DATA WITH AN API
+  // const posts = await getData();
 
-  // console.log("first0", posts);
+  // FETCH DATA WITHOUT AN API
+  const posts = await getPosts();
+
   return (
     <div className={styles.container}>
       {posts.map((post) => {
