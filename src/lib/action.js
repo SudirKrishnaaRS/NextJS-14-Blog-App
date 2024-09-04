2f760e1f92966202e61f8c66d609dd4851f2ed0d
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-09-04 12:54:37 +0530

    Encrypted Pwd using bcryt package

--- src/lib/action.js
+++ src/lib/action.js
@@ -6,6 +6,7 @@ import { revalidatePath } from "next/cache";
 import { Post, User } from "./models";
 import { connectToDb } from "./utils";
 import { signIn, signOut } from "./auth";
+import bcrypt from "bcrypt";
 
 // addPost() - To add a new Post (Server action)
 export const addPost = async (formData) => {
@@ -86,10 +87,14 @@ export const register = async (formData) => {
       return "Username already exists";
     }
 
+    // Encrypt the password using `bcrypt` package
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       img,
     });
     // HINT : All these username, email, password, passwordRepeat, img are the same string value given to the `name` in input fields
