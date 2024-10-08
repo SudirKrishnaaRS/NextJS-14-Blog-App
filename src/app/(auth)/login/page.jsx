254aaf06e77a0949a9295fe543bc68ad33c7705a
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-10-08 13:36:23 +0530

    Updated login with LoginForm comp

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,12 +1,22 @@
-import { handleGithubLogin } from "@/lib/action";
 import React from "react";
+import LoginForm from "@/components/loginForm/loginForm";
+import { handleGithubLogin } from "@/lib/action";
+import { FaGithub } from "react-icons/fa";
+import styles from "./login.module.css";
 
-const LoginPage = async () => {
+const LoginPage = () => {
   return (
-    <div>
-      <form action={handleGithubLogin}>
-        <button>Login with Github</button>
-      </form>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form action={handleGithubLogin}>
+          <button className={styles.github}>
+            <div className={styles.githubButton}>
+              <FaGithub size="30" title="github logo" /> Login with Github
+            </div>
+          </button>
+        </form>
+        <LoginForm />
+      </div>
     </div>
   );
 };
