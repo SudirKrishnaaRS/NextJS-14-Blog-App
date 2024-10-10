bac77f8ef34daa01513228938ccc05cc677ba2ba
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-10-10 12:23:24 +0530

    Added adminUserForm comp logic

--- src/components/adminUserForm/adminUserForm.jsx
+++ src/components/adminUserForm/adminUserForm.jsx
@@ -1,7 +1,27 @@
+"use client";
+import { useFormState } from "react-dom";
+import { addUser } from "@/lib/action";
 import styles from "./adminUserForm.module.css";
 
 const AdminUserForm = () => {
-  return <div className={styles.container}>AdminUserForm</div>;
+  const [state, formAction] = useFormState(addUser, undefined);
+
+  return (
+    <form action={formAction} className={styles.container}>
+      <h1>Add New User</h1>
+      <input type="text" name="username" placeholder="username" />
+      <input type="text" name="email" placeholder="email" />
+      <input type="password" name="password" placeholder="password" />
+      <input type="text" name="img" placeholder="img" />
+      <select name="isAdmin">
+        <option value="false">Is Admin?</option> {/* Placeholder */}
+        <option value="false">User</option>
+        <option value="true">Admin</option>
+      </select>
+      <button>Add</button>
+      {state && state.error}
+    </form>
+  );
 };
 
 export default AdminUserForm;
