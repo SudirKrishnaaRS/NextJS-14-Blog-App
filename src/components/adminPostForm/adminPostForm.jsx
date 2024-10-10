962e32a1ddda76b22307396859e8063675cf8784
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-10-10 11:48:40 +0530

    Add new post functionality

--- src/components/adminPostForm/adminPostForm.jsx
+++ src/components/adminPostForm/adminPostForm.jsx
@@ -1,7 +1,23 @@
+"use client";
+import { useFormState } from "react-dom";
+import { addPost } from "@/lib/action";
 import styles from "./adminPostForm.module.css";
 
-const AdminPostForm = () => {
-  return <div className={styles.container}>AdminPostForm</div>;
+const AdminPostForm = ({ userId }) => {
+  const [state, formAction] = useFormState(addPost, undefined);
+
+  return (
+    <form action={formAction} className={styles.container}>
+      <h1>Add New Post</h1>
+      <input type="hidden" name="userId" value={userId} />
+      <input type="text" name="title" placeholder="title" />
+      <input type="text" name="slug" placeholder="slug" />
+      <input type="text" name="img" placeholder="img" />
+      <textarea type="text" name="desc" placeholder="desc" rows={10} />
+      <button>Add</button>
+      {state && state.error}
+    </form>
+  );
 };
 
 export default AdminPostForm;
