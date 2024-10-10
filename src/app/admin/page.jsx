3f47d5902243e95f7512a5f18e87b5347463932b
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-10-10 11:49:20 +0530

    Pass loggedIn userId to the props from the session

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -4,8 +4,11 @@ import AdminPosts from "@/components/adminPosts/adminPosts";
 import AdminPostForm from "@/components/adminPostForm/adminPostForm";
 import AdminUsers from "@/components/adminUsers/adminUsers";
 import AdminUserForm from "@/components/adminUserForm/adminUserForm";
+import { auth } from "@/lib/auth";
 
-const AdminPage = () => {
+const AdminPage = async () => {
+  const session = await auth();
+
   return (
     <div className={styles.container}>
       <div className={styles.row}>
@@ -15,14 +18,14 @@ const AdminPage = () => {
           </Suspense>
         </div>
         <div className={styles.col}>
-          <AdminPostForm />
+          <AdminPostForm userId={session?.user.id} />
         </div>
       </div>
 
       <div className={styles.row}>
         <div className={styles.col}>
           <Suspense fallback={<div>Loading...</div>}>
-            <AdminUsers />
+            <AdminUsers userId={session?.user.id} />
           </Suspense>
         </div>
         <div className={styles.col}>
